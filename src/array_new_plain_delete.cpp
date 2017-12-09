cc3f8753e3b07272219cc28727edf9171d3dd2ad
Yoonsoo Kim <yesarang.kim@gmail.com>
2017-12-09 12:23:01 -0800

    Added array new/plain delete example

---
+++ src/array_new_plain_delete.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+struct A {
+	A() {
+		cout << __FUNCTION__ << " called for " << id_ << endl;
+	}
+
+	~A() {
+		cout << __FUNCTION__ << " called for " << id_ << endl;
+	}
+
+	static int id;
+	static bool throwEx;
+	int id_ = id++;
+};
+
+int A::id = 0;
+
+struct B : public A {
+	B() {
+		cout << __FUNCTION__ << " called for " << id_ << endl;
+	}
+
+	~B() {
+		cout << __FUNCTION__ << " called for " << id_ << endl;
+	}
+};
+
+int main() {
+	A* pA{ new A[3] };
+	B* pB{ new B[3] };
+
+	delete pA;
+	delete pB;
+}
+
