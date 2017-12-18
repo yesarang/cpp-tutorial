592c3a7c41ef698c749b6984ff9da6d18dba8259
Yoonsoo Kim <yesarang.kim@gmail.com>
2017-12-17 20:25:01 -0800

    simplified the example

--- src/array_new_delete.cpp
+++ src/array_new_delete.cpp
@@ -18,18 +18,9 @@ struct A {
 
 int A::id = 0;
 
-struct B : public A {
-	B() {
-		cout << __FUNCTION__ << " called for " << id_ << endl;
-	}
-
-	~B() {
-		cout << __FUNCTION__ << " called for " << id_ << endl;
-	}
-};
-
 int main() {
-	unique_ptr<A[]> pA{ new A[3] };
-	unique_ptr<B[]> pB{ new B[3] };
+	A* pA{ new A[3] };
+
+	delete[] pA;
 }
 
